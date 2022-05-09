b80bbe398d218005fdd9b328e5c80c3afd0e02d7
L1ndseyHerman <lherman21@jcu.edu>
2022-05-09 01:32:40 -0400

    This could work if sticking to 16:9 aspect ratio

--- src/the-adventures-of-bob/ScreenSizeTest.tsx
+++ src/the-adventures-of-bob/ScreenSizeTest.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import bob from "../pages/bobseventy.png";
 import classes from "./ScreenSizeTest.module.css";
 
 const ScreenSizeTest: React.FC = () => {
   return (
     <div className={classes.gameDiv}>
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
       <img src={bob} alt="Bob" className={classes.gridImage} />
       <img src={bob} alt="Bob" className={classes.gridImage} />
       <img src={bob} alt="Bob" className={classes.gridImage} />
