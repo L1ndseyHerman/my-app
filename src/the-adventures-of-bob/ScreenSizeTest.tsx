412a2b1c11b2cc4dfd4b01f4346217b6a0e5b3a0
L1ndseyHerman <lherman21@jcu.edu>
2022-05-09 20:58:07 -0400

    Gaps when screen resizes basically the website version of problems I had in Android :(

--- src/the-adventures-of-bob/ScreenSizeTest.tsx
+++ src/the-adventures-of-bob/ScreenSizeTest.tsx
@@ -6,21 +6,15 @@ const ScreenSizeTest: React.FC = () => {
   return (
     <div className={classes.gameDiv}>
       <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
-      <img src={bob} alt="Bob" className={classes.gridImage} />
+      <img src={bob} alt="Bob" className={classes.gridImageColumn2} />
+      <img src={bob} alt="Bob" className={classes.gridImageRow2} />
+      <img src={bob} alt="Bob" className={classes.gridImageRow2Column2} />
+      <img src={bob} alt="Bob" className={classes.gridImageRow3} />
+      <img src={bob} alt="Bob" className={classes.gridImageRow4} />
+      <img src={bob} alt="Bob" className={classes.gridImageRow5} />
+      <img src={bob} alt="Bob" className={classes.gridImageRow6} />
+      <img src={bob} alt="Bob" className={classes.gridImageRow7} />
+      <img src={bob} alt="Bob" className={classes.gridImageRow8} />
     </div>
   );
 };
